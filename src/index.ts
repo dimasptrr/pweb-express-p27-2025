90ebceb3bb31b16d2b326f334219f174c847329f
MasRozak <mygdrives01@gmail.com>
2025-11-08 00:11:28 +0700

    Add Cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoutes from "./routes/auth.route";
 import libraryRoutes from "./routes/library.route";
 import genreRoutes from "./routes/genre.route";
@@ -8,9 +9,13 @@ import transactionRoutes from "./routes/transaction.route";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Middleware
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
